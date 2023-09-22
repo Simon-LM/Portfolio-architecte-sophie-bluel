3df26d3c999159a8ccf8ff49b1d40b8beeb4e8d1
Simon-LM <simon.ln.dev@pm.me>
2023-09-22 21:23:36 +0200

    Initial

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,13 @@
+console.log("Page JAvaScripte")
+
+/* const myImage = document.querySelector("img"); */
+
+/* fetch("flowers.jpg")
+  .then(function (response) {
+    return response.blob();
+  })
+  .then(function (myBlob) {
+    const objectURL = URL.createObjectURL(myBlob);
+    myImage.src = objectURL;
+  }); */
+
