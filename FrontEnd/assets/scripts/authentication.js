b9361bf5143f4dcb586c67361adcc9d27d9bdea2
Simon-LM <simon.ln.dev@pm.me>
2023-09-29 18:07:38 +0200

    Authentification with error message | DONE

--- FrontEnd/assets/scripts/authentication.js
+++ FrontEnd/assets/scripts/authentication.js
@@ -1,75 +1,80 @@
 console.log("Page JAvaScripte | AUTHENTICATION")
 
-// Password visibility ON/OFF
+// Password visibility ON/OFF //
 const eye = document.querySelector(".icon-eye");
 const eyeoff = document.querySelector(".icon-eye-off");
 const passwordField = document.querySelector("input[type=password]");
-/* eye.addEventListener("click", () => {
-    eye.style.display = "none";
-    eyeoff.style.display = "block";
-    passwordField.type = "text";
-});  */ 
 eyeoff.addEventListener("click", () => {
     eyeoff.style.display = "none";
     eye.style.display = "block";
     passwordField.type = "text";
+    console.log("Click to make the password visible")
 });
 eye.addEventListener("click", () => {
     eye.style.display = "none";
     eyeoff.style.display = "block";
     passwordField.type = "password";
+    console.log("Click for hidden the password")
 });  
-
-// export function ajoutListenerEnvoyerAvis() {
-//     const formulaireAvis = document.querySelector(".formulaire-avis");
-//     formulaireAvis.addEventListener("submit", function (event) {
-//     /* ... */
-//     });
-// }
  
-// import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
-// // Récupération des pièces depuis le fichier JSON
-// const reponse = await fetch('http://localhost:8081/pieces/');
-// const pieces = await reponse.json();
-// // on appelle la fonction pour ajouter le listener au formulaire
-// ajoutListenerEnvoyerAvis()
+// AUTHENTICATION // 
+let submit = document.querySelector("#authentication")
+submit.addEventListener("submit", function (event) {
+    console.log("click on submit")
+    event.preventDefault()  // Désactivation du comportement par défaut du navigateur
+    const login = {
+        email: document.getElementById("email-id").value,
+        password: document.getElementById("password-id").value,
+    }
+    const toLogin = JSON.stringify(login)
+    console.log(toLogin)   
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: toLogin,
+    })
+    .then((responseAPI) => {
+        if (!responseAPI.ok) { 
+            if (responseAPI.status === 404) {
+                alert("Adresse email ou identifiant incorrect")
+                console.log(responseAPI.status)
+                console.log("Adresse email ou identifiant incorrect")
+                return
+            }
+            if (responseAPI.status === 401) {
+                alert("Mot de passe incorrect")
+                console.log(responseAPI.status)
+                console.log("Mot de passe incorrect")
+                return
+            }
+            else {
+                alert("Adresse email ou mot de passe non valide")
+                console.log(responseAPI.status)
+                console.log("Adresse email ou mot de passe non valide")
+                return
+            }
+        }
+        else {
+            return responseAPI.json()
+            .then((jsonResponseAPI) => {
+                if (!jsonResponseAPI.token) {
+                    console.log("A token is missing")
+                    console.log(responseAPI.status)
+                }
+                if (jsonResponseAPI.token) {
+                    const token = JSON.stringify(jsonResponseAPI.token)
+                    console.log(responseAPI.status)
+                    console.log(jsonResponseAPI)
+                    console.log(token)
+                    window.localStorage.setItem("token", token)
+                    window.location = "../index.html"
+                }
+                else {
+                    console.log("Unidentified connection error, but it is neither email, password, nor token abscense.")
+                    console.log(responseAPI.status)
+                }
+            })
+        }
+    })
+})
 
-// Désactivation du comportement par défaut du navigateur
-// event.preventDefault();
-
-// Création de l’objet du nouvel avis.
-/* const login = {
-    email: event.target.querySelector("[name=email-id").value,
-    password: event.target.querySelector("[name=password-id]").value,
- }; */
- // Création de la charge utile au format JSON
-/* const toLogin = JSON.stringify(login); */
-// Appel de la fonction fetch avec toutes les informations nécessaires
-/* fetch("http://localhost:5678/api-docs/users/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: toLogin
-}); */
-// body: '{ "email": "sophie.bluel@test.tld","password": "S0phie"}'
-
-// export function ajoutListenerEnvoyerAvis() {
-//     const formulaireAvis = document.querySelector(".formulaire-avis");
-//     formulaireAvis.addEventListener("submit", function (event) {
-//     /* ... */
-//     });
-// }
- 
-// import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
-// // Récupération des pièces depuis le fichier JSON
-// const reponse = await fetch('http://localhost:8081/pieces/');
-// const pieces = await reponse.json();
-// // on appelle la fonction pour ajouter le listener au formulaire
-// ajoutListenerEnvoyerAvis()
-
-
-
-// fetch("api-docs/users/login", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: '{ "email": "sophie.bluel@test.tld","password": "S0phie"}'
-// });
--- FrontEnd/assets/scripts/galleries.js
+++ FrontEnd/assets/scripts/galleries.js
@@ -89,3 +89,5 @@ fetch(urlCategoriesAPI)
 .catch(function (error) {
   console.log("Il y a eu un problème avec l'opération fetch : " + error.message,)
 })
+
+
